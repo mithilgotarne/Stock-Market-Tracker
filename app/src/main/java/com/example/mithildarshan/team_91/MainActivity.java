f1220bd1774d9c29a1d747b3a277bd431ca45aa0
mithishri <mithilgotarne@gmail.com>
2017-01-08 14:18:07 +0530

    list view changes

--- app/src/main/java/com/example/mithildarshan/team_91/MainActivity.java
+++ app/src/main/java/com/example/mithildarshan/team_91/MainActivity.java
@@ -3,21 +3,33 @@ package com.example.mithildarshan.team_91;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mithildarshan.team_91.adapter.StockAdapter;
 import com.example.mithildarshan.team_91.background.BackgroundService;
 import com.example.mithildarshan.team_91.background.FetchDailyData;
-import com.example.mithildarshan.team_91.model.Company;
-import com.example.mithildarshan.team_91.model.Record;
+import com.example.mithildarshan.team_91.background.RefreshService;
+import com.example.mithildarshan.team_91.background.RefreshTask;
 import com.example.mithildarshan.team_91.model.Stock;
+import com.example.mithildarshan.team_91.util.Utils;
 
 import io.realm.Realm;
-import io.realm.RealmList;
 import io.realm.RealmResults;
+
+import static com.example.mithildarshan.team_91.util.Utils.KEY_FIRST_START;
+import static com.example.mithildarshan.team_91.util.Utils.KEY_REF_RATE;
+import static com.example.mithildarshan.team_91.util.Utils.PREFS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private Realm realm;
 
     private static final int JOB_ID = 100;
+    private SharedPreferences prefs;
     private JobScheduler mJobScheduler;
     private RealmResults<Stock> stocks;
 
@@ -32,8 +45,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        prefs = getSharedPreferences(PREFS, MODE_PRIVATE);
+        mJobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        checkFirstStart();
         //realm = Realm.getDefaultInstance();
-        new FetchDailyData(this).execute();
+        //new FetchDailyData(this).execute();
 
         /*txt = (TextView) findViewById(R.id.text_view);
         txt.setText(" ");
@@ -67,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
             */
 
 
-       // mJobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        //
         //constructJob();
 
        /* SharedPreferences prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE);
@@ -77,22 +95,125 @@ public class MainActivity extends AppCompatActivity {
 
         //constructJob();
 
-        /*Realm realm = Realm.getDefaultInstance();
-
+        Realm realm = Realm.getDefaultInstance();
         stocks = realm.where(Stock.class).findAll();
-
         StockAdapter stockAdapter = new StockAdapter(MainActivity.this, stocks);
-
         ListView listView = (ListView) findViewById(R.id.stock_listview);
         listView.setAdapter(stockAdapter);
-*/
+
+        createRefreshJob(prefs.getInt(KEY_REF_RATE, 1));
+
+    }
+
+    private void checkFirstStart() {
+
+        if (!prefs.contains(KEY_FIRST_START)) {
+
+            new FetchDailyData() {
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+                    prefs.edit().putBoolean(KEY_FIRST_START, false).apply();
+                }
+            }.execute();
+
+            constructJob();
+        }
     }
 
     private void constructJob() {
         JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, new ComponentName(this, BackgroundService.class));
-        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY).setPeriodic(1000).setPersisted(true);
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setPeriodic(24 * 60 * 60 * 1000);
         mJobScheduler.schedule(builder.build());
     }
+
+    private void createRefreshJob(int refreshRate) {
+        JobInfo.Builder builder = new JobInfo.Builder(200, new ComponentName(this, RefreshService.class));
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY)
+                .setPeriodic(refreshRate * 60 * 1000);
+        mJobScheduler.schedule(builder.build());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+
+                if (Utils.isMarketClosed()) {
+
+                    Toast.makeText(MainActivity.this, "Market Closed", Toast.LENGTH_SHORT).show();
+                    return true;
+
+                }
+
+                new RefreshTask() {
+                    @Override
+                    protected void onPostExecute(Boolean aBoolean) {
+                        Toast.makeText(MainActivity.this, "Refreshing", Toast.LENGTH_SHORT).show();
+                    }
+                }.execute();
+
+                return true;
+            case R.id.action_refresh_rate:
+                createDialog();
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createDialog() {
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setMessage("Enter Refresh Rate in minutes");
+        alert.setTitle("Refresh Rate");
+
+        int min = prefs.getInt(KEY_REF_RATE, 1);
+
+
+        View view = getLayoutInflater().inflate(R.layout.dialogedit, null);
+        final EditText edittext = (EditText) view.findViewById(R.id.rate_edittext);
+        edittext.setText(String.valueOf(min));
+
+        alert.setView(view);
+
+        alert.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String YouEditTextValue = edittext.getText().toString();
+                if (!TextUtils.isEmpty(YouEditTextValue)) {
+                    if (TextUtils.isDigitsOnly(YouEditTextValue)) {
+                        if (Integer.parseInt(YouEditTextValue) > 1) {
+                            prefs.edit().putInt(KEY_REF_RATE, Integer.parseInt(YouEditTextValue)).apply();
+                            createRefreshJob(Integer.parseInt(YouEditTextValue));
+                        } else
+
+                            Toast.makeText(MainActivity.this, "Invalid Input", Toast.LENGTH_SHORT).show();
+                    } else
+                        Toast.makeText(MainActivity.this, "Invalid Input", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid Input", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // what ever you want to do with No option.
+            }
+        });
+
+        alert.show();
+
+    }
+
 
     /*private void getData() {
 
